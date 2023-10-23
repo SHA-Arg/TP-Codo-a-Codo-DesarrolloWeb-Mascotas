5ddf17783a6fb9faa1bb021ace7d3666fed62428
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2023-10-22 23:14:33 -0400

    update method to get values from adopt form

--- js/apadrinar.js
+++ js/apadrinar.js
@@ -2,6 +2,8 @@ const sponsorForm = document.forms["sponsor-form"];
 
 sponsorForm.onsubmit = (e) => {
     e.preventDefault();
+
+    let formData = new FormData(formEl);
     Swal.fire( 
         'Gracias', 
         'Su solicitud fue enviada con éxito', 
